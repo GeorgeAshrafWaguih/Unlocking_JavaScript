9b3d49d9c295e277eb8601e1e6c65e5072afe540
George <George.ashraf@hotmail.com>
2023-03-20 03:26:35 +0200

    Adding arithmatics

--- main.js
+++ main.js
@@ -43,3 +43,115 @@ console.log(typeof true) // bolean
 console.log(typeof false) // bolean
 console.log(typeof undefined)  // undefined
 console.log(typeof null)    // object
+
+/**
+ * Variables 
+ */
+
+var name = "Geo";
+var age = 33
+console.log(name)
+console.log(name + " "+ age);
+console.log(hello)
+hello.innerHTML = "Option"
+
+var user = "Geo"
+var User = "Buddy"
+console.log(user)
+console.log(User)
+
+console.log("This is a \"School\"")
+console.log("This \
+is a \
+School")
+console.log("Welcome\nBack")
+
+document.write(name + " "+ age)
+
+/**
+ * Template Literals (Template strings)
+ */
+
+let a = "We love"
+let b = "JavaScript"
+let c = "and"
+let d = "Programing"
+console.log(a + " "+ b + " " + c + " " + d)
+console.log(a + " "+ b + "\n" + c + " " + d)
+console.log(`${a} "${b}"
+${c} "${d}"`)
+
+let title = "Hello"
+let desc = "This is the paragraph"
+let markUp = `
+    <div class="card">
+        <div class="child">
+            <h2>${title}</h2>
+            <p>${desc}</p>
+        </div>
+    </div>
+`
+document.write(markUp)
+
+let title1 = "Geo"
+let desc1 = "Geo Web School"
+let date = "25/10"
+
+let card = `
+    <div class="card">
+        <div class="child">
+            <h3>Hello ${title1}</h2>
+            <p>${desc1}</p>
+            <span>${date}</span>
+        </div>
+    </div>
+`
+
+document.write(card.repeat(4));
+
+/**
+ * Arithmetic operators
+ */
+
+console.log(10*20);
+console.log(10+"G");
+console.log(10*"G");
+console.log(typeof NaN);
+console.log(2**4);
+console.log(2*2*2*2);
+console.log(11%2);
+
+console.log(+100);
+console.log(+"100");
+console.log(+"Geo");
+console.log(+"0xff");
+console.log(+null);
+console.log(+true);
+console.log(+false);
+
+console.log(-100);
+console.log(-"100");
+console.log(-"-100");
+console.log(-0xff);
+console.log(-null);
+console.log(-true);
+console.log(-false);
+
+console.log(Number("100"));
+
+/**
+ * Type Coercion 
+ */
+
+let x = "10";
+let y = 20;
+let z = true;
+console.log(x+y);
+console.log(+x+y);
+console.log(x-y);
+console.log(""-2);
+console.log(+"");
+console.log(false-true);
+console.log(+false-+true);
+console.log(x+y+z);
+console.log(+x+y+z);
